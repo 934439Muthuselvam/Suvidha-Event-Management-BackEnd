3817ab6ff0c90b5a6040e1b1e0ff936de21c293a
934439Muthuselvam <muthuselvam10102002@gmail.com>
2025-01-04 21:23:50 +0530

    dp modified 04/01/25

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-export const conn = mongoose.createConnection('mongodb://0.0.0.0:27017/NOTOMAS', (err, db) => {
+export const conn = mongoose.createConnection('mongodb://0.0.0.0:27017/savidha', (err, db) => {
   if (err) {
     console.log(err)
   }
