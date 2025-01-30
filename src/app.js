4cb89cb4514069c0f2b423f8a7399b933fa9d8d1
934439Muthuselvam <muthuselvam10102002@gmail.com>
2025-01-31 03:30:28 +0530

    Muthu 30/01/25

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 }))
 
 app.use('/', router)
-app.get('/app', (req, res) => res.send('Welcome to Natomas Group'))
+app.get('/app', (req, res) => res.send('Welcome to Suvidha Foundation'))
 export default app
 
 
