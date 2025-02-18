f8014589622785e680d22822f3aaa5549ca67eba
934439Muthuselvam <muthuselvam10102002@gmail.com>
2025-02-19 02:27:38 +0530

    Muthu vercel url 19/02/25

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -1,12 +1,7 @@
-import mongoose from "mongoose";
-
-export const db = mongoose
-  .createConnection(
-    "mongodb+srv://muthuselvam73bitf:1234@cluster0.0jr7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .on("error", (err) => {
-    console.error("MongoDB Connection Error:", err);
-  })
-  .once("open", () => {
-    console.log("Master Data Dictionary Database Connected successfully");
-  });
+import mongoose from 'mongoose'
+export const conn = mongoose.createConnection("mongodb+srv://muthuselvam73bitf:1234@cluster0.0jr7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", (err, db) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log('Master Data Dictionary Database Connected successfully')
+})
